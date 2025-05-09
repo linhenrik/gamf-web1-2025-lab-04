e1a4e1d6e6659ced31f0e475f10d1fb7101b0f65
linhenrik <linhenrik@gmail.com>
2025-05-09 11:24:45 +0200

    commit

--- html/js/scripts/db.js
+++ html/js/scripts/db.js
@@ -3,3 +3,10 @@ const apiUrl = 'https://nettuts.hu/jms/linhenrik/'
 export const getAll = (entity = 'users') => {
     return fetch(apiUrl + entity).then(res => res.json());
 };
+
+export const remove = (id = 0, entity = 'products') => {
+    return fetch(`${serverUrl}${entity}/${id}`, {
+        method: 'DELETE',
+    })
+    .then( response => response.json() );
+};
